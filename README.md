9c88720178fd9446958db2a4450117bec2d33a71
Dragos <ginjoveanu.dragos@gmail.com>
2021-08-24 15:20:28 +0300

    added readMe, converted components to functional components, added separate components for adding/searching words

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # dictionary-react
 
-The user can add words to a 'dictionary' by submiting a form.  
-The app was created with 'create-react-app' command.
+The user can add words to a 'dictionary' by submiting a form.   
+The app was created with 'create-react-app' command.   
--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
+function AddWord(props) {
+  const addWord = (e) => {
+    e.preventDefault();
+    const currentWord = props.currentWord;
+    let words = props.words;
+    if (!words.includes(currentWord)) {
+      props.addWord(currentWord);
+      props.changeDuplicate();
+    } else {
+      props.changeDuplicate(true);
+    }
+  }
+  
+  return(
+    <input type="submit" value="Add word" onClick={addWord}/>
+  );
+}
+
+function SearchWord(props) {
+  const searchWord = (e) => {
+    e.preventDefault();
+    const currentWord = props.currentWord;
+    let words = props.words;
+    if (words.includes(currentWord)) {
+      props.changeDuplicate(true);
+    } else {
+      props.changeDuplicate(false);
+    }
+  }
+  
+  return(
+    <input type="submit" value="Search word" onClick={searchWord}/>
+  );
+}
 
 function WordList(props) {
   const words = props.words;
-  console.log(words);
   const listWords = words.map((word) =>
     <li key={word.toString()}>
       {word}
@@ -17,53 +51,45 @@ function WordList(props) {
   );
 }
 
-class Dictionary extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentWord: '',
-      duplicate: false,
-      words: []
-    }
+function Dictionary() {
+  const [currentWord, setCurrentWord] = useState();
+  const [words, setWords] = useState([]);
+  const [duplicate, setDuplicate] = useState();
+
+  const changeCurrentWord = (e) => {
+    setCurrentWord(e.target.value);
   }
 
-  addCurrentWord = (event) => {
-    this.setState({ currentWord: event.target.value});
-  }
-  
-  handleSubmit = (event) => {
-    const currentWord = this.state.currentWord;
-    let words = this.state.words;
-    if (!words.includes(currentWord)) {
-      words.push(currentWord);
-      this.setState({duplicate: false});
-    } else {
-      this.setState({duplicate: true});
-    }
-    this.setState({words: words});
-    event.preventDefault();
+  const changeDuplicate = (status) => {
+    setDuplicate(status);
   }
 
-  render() {
-    let alert;
-    if (this.state.duplicate) {
-      alert = <h5>The word is already in the list</h5>
-    }
-    return (
-      <div>
-        {alert}
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Word name:
-            <input type="text" value={this.state.value} onChange={this.addCurrentWord} />
-          </label>
-          <input type="submit" value="Add word" />
-        </form>
-        <WordList words = {this.state.words}/>
-      </div>
-    );
+  const addWord = (word) => {
+    words.push(word);
+    setWords(words)
+    //WordList not updating
   }
+
+  let alert;
+  if (duplicate === true) {
+    alert = <h5>The word is in the list</h5>
+  } else if(duplicate === false){
+    alert = <h5>The word is not in the list</h5>
+  }
+  return(
+    <div>
+      {alert}
+      <form >
+        <label>
+          Word name:
+          <input type="text" onChange={changeCurrentWord}/>
+        </label>
+        <AddWord changeDuplicate = {changeDuplicate} addWord = {addWord} currentWord = {currentWord} words = {words}/>
+        <SearchWord changeDuplicate = {changeDuplicate} currentWord = {currentWord} words = {words}/>
+        <WordList words = {words}/>
+      </form>
+    </div>
+  );
 }
 
 ReactDOM.render(
